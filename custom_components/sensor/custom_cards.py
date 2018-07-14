d5721557172ac00ee175d2a0b0b08fa663b71b51
Marius <ciotlos@gmail.com>
2018-07-14 19:48:28 +0300

    Bugfixes and more logging (#5)
    * Corrected bug with unused cards
    
    Simplified logic and added a bit more verbose logging
    
    Reverted dev value
    
    * Update to this PR
    
    - Added usage verification to get_installed_cards
    - Version bump
    - Only update if remote version is higher
    -
    
    * Stage 1 refactoring
    
    * Addec check version service and increased throttle
    
    * Added sensor as well with auto discovery
    
    * Fixes for auto updates
    
    * Removed param as not used for now
    
    * Allow dynamic removal and adding of cards
    
    * More logging
    
    * Forced state to change every time to force ui to referesh on state change for attributes
    
    * Forced state to change every time to force ui to referesh on state change for attributes
    
    * Fixed merge conflicts
    
    * Updated sensor with merge request

--- custom_components/sensor/custom_cards.py
+++ custom_components/sensor/custom_cards.py
@@ -4,20 +4,24 @@ Get updates about your custom_cards.
 For more details about this component, please refer to the documentation at
 https://github.com/custom-components/sensor.custom_cards
 """
+import logging
+import time
 from datetime import timedelta
 from homeassistant.helpers.entity import Entity
 from custom_components.custom_cards import DATA_CC, SIGNAL_SENSOR_UPDATE
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 import custom_components.custom_cards as cc
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 DEPENDENCIES = ['custom_cards']
 
 SCAN_INTERVAL = timedelta(seconds=60)
+_LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Create the sensor"""
+    _LOGGER.info('Sensor %s version %s is starting', __version__, __name__.split('.')[1])
     add_devices([CustomCards(hass)])
 
 class CustomCards(Entity):
@@ -26,7 +30,7 @@ class CustomCards(Entity):
     def __init__(self, hass):
         """Initialize the sensor."""
         self.hass = hass
-        self._state = None
+        self._state = time.time()
         self._attributes = self.hass.data[DATA_CC]
 
     async def async_added_to_hass(self):
@@ -36,9 +40,10 @@ class CustomCards(Entity):
 
     def _update_callback(self):
         """Method to update sensor value"""
-        self._state = 'Active'
         self._attributes = self.hass.data[DATA_CC]
-        self.async_schedule_update_ha_state()
+        self._state = time.time()
+        _LOGGER.debug('Sensor update for signal %s', self._attributes)
+        self.async_schedule_update_ha_state(True)
 
     @property
     def name(self):
@@ -48,7 +53,7 @@ class CustomCards(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return 'Active'
+        return self._state
 
     @property
     def device_state_attributes(self):
