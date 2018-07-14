0e0aa8c1f4e0eea9a0e675b6faeaa2b456f6cb51
Marius <ciotlos@gmail.com>
2018-07-14 17:52:53 +0300

    Combined with sensor and allows local caching (#4)
    * Corrected bug with unused cards
    
    Simplified logic and added a bit more verbose logging
    
    Reverted dev value
    
    * Update to this PR
    
    - Added usage verification to get_installed_cards
    - Version bump
    - Only update if remote version is higher
    -
    
    * Stage 1 refactoring
    
    * Addec check version service and increased throttle
    
    * Added sensor as well with auto discovery
    
    * Fixes for auto updates
    
    * Removed param as not used for now
    
    * Allow dynamic removal and adding of cards

--- custom_components/custom_cards.py
+++ custom_components/custom_cards.py
@@ -13,15 +13,19 @@ import requests
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import track_time_interval
+from homeassistant.helpers.discovery import load_platform
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 __version__ = '1.1.6'
 
 DOMAIN = 'custom_cards'
+DATA_CC = 'custom_cards_data'
 CONF_AUTO_UPDATE = 'auto_update'
+SIGNAL_SENSOR_UPDATE = 'custom_cards_update'
 
 ATTR_CARD = 'card'
 
-INTERVAL = timedelta(minutes=60)
+INTERVAL = timedelta(days=1)
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -31,120 +35,143 @@ CONFIG_SCHEMA = vol.Schema({
 
 _LOGGER = logging.getLogger(__name__)
 
-BASE_URL = 'https://raw.githubusercontent.com/'
-REPO = 'ciotlosm/custom-lovelace/master/'
-BROWSE_REPO = 'https//github.com/' + REPO
-BASE_REPO = BASE_URL + REPO
-
+BROWSE_REPO = 'https//github.com/ciotlosm/custom-lovelace/master/'
+BASE_REPO = 'https://raw.githubusercontent.com/ciotlosm/custom-lovelace/master/'
 
 def setup(hass, config):
     """Set up the component."""
-    _LOGGER.info('version %s is starting, if you have ANY issues with this, please report them here: https://github.com/custom-components/%s', __version__, __name__.split('.')[1])
-    auto_update = config[DOMAIN][CONF_AUTO_UPDATE]
+    _LOGGER.info('version %s is starting, if you have ANY issues with this, please report \
+                  them here: https://github.com/custom-components/%s', __version__, __name__.split('.')[1])
     www_dir = str(hass.config.path("www/"))
     lovelace_config = str(hass.config.path("ui-lovelace.yaml"))
-    data_card = None
-
-    def update_cards_interval(now):
-        """Set up recuring update."""
-        _update_cards(www_dir, lovelace_config, data_card, auto_update)
+    controller = CustomCards(hass, www_dir, lovelace_config)
 
     def update_cards_service(call):
         """Set up service for manual trigger."""
-        data_card = call.data.get(ATTR_CARD)
-        _update_cards(www_dir, lovelace_config, data_card, True)
+        controller.update_cards()
 
-    track_time_interval(hass, update_cards_interval, INTERVAL)
+    def update_card_service(call):
+        """Set up service for manual trigger."""
+        controller.update_card(call.data.get(ATTR_CARD))
+
+    track_time_interval(hass, controller.cache_versions, INTERVAL)
     hass.services.register(
         DOMAIN, 'update_cards', update_cards_service)
+    hass.services.register(
+        DOMAIN, 'update_card', update_card_service)
+    hass.services.register(
+        DOMAIN, 'check_versions', controller.cache_versions)
+    load_platform(hass, 'sensor', DOMAIN)
     return True
 
-def _update_cards(www_dir, lovelace_config, data_card, force_update):
-    """Update cards."""
-    updates = []
-    if data_card == None:
-        cards = get_installed_cards(www_dir, lovelace_config)
-    else:
-        cards = [data_card]
-    if cards != None:
-        for card in cards:
-            localversion = get_local_version(card, lovelace_config)
-            remoteversion = get_remote_version(card)
-            if localversion != False and remoteversion != False and remoteversion != localversion:
-                if force_update:
-                    download_card(card, www_dir)
-                    update_config(lovelace_config, card, localversion, remoteversion)
-                    _LOGGER.info('Upgrade of %s from version %s to version %s complete', card, localversion, remoteversion)
-                else:
-                    updates.append(card + ' (' + localversion + '|' +remoteversion + ')')
-            else:
-                _LOGGER.debug('Card %s is version %s and remote is version %s, skipping update..', card, localversion, remoteversion)
-        if updates:
-            _LOGGER.info("Available updates found for cards (localversion|remoteversion) %s. Please run the service 'custom_cards.update_cards' to upgrade, or visit %s to download manually", updates, BROWSE_REPO)
 
-def get_installed_cards(www_dir, lovelace_config):
-    """Get all cards in use from the www dir"""
-    _LOGGER.debug('Checking for installed cards in  %s', www_dir)
-    cards = []
-    cards_in_use = []
-    for file in os.listdir(www_dir):
-        if file.endswith(".js"):
-            cards.append(file.split('.')[0])
-    if len(cards):
-        _LOGGER.debug('Checking which cards that are in use in ui-lovelace.yaml')
-        for card in cards:
-            with open(lovelace_config, 'r') as local:
-                for line in local.readlines():
-                    if '/' + card + '.js' in line:
-                        cards_in_use.append(card)
-                        break
-        _LOGGER.debug('These cards where found: %s', cards_in_use)
-    else:
-        _LOGGER.debug('No cards where found. %s', cards)
-        cards_in_use = None
-    return cards_in_use
+class CustomCards:
+    """Custom cards controller."""
+    def __init__(self, hass, www_dir, lovelace_config):
+        self.hass = hass
+        self.www_dir = www_dir
+        self.lovelace_config = lovelace_config
+        self.cards = None
+        self.hass.data[DATA_CC] = {}
+        self.cache_versions(None) # Force a cache update on startup
 
-def get_remote_version(card):
-    """Return the remote version if any."""
-    remoteversion = BASE_REPO + card + '/VERSION'
-    response = requests.get(remoteversion)
-    if response.status_code == 200:
-        remoteversion = response.text
-        _LOGGER.debug('Remote version of %s is %s', card, remoteversion)
-        remoteversion = remoteversion
-    else:
-        _LOGGER.debug('Could not get the remote version for %s', card)
-        remoteversion = False
-    return remoteversion
+    def cache_versions(self, time):
+        self.cards = self.get_installed_cards()
+        self.hass.data[DATA_CC] = {} # Empty list to start from scratch
+        for card in self.cards:
+            localversion = self.get_local_version(card)
+            remoteversion = self.get_remote_version(card)
+            has_update = (localversion != False and remoteversion != False and remoteversion != localversion)
+            self.hass.data[DATA_CC][card] = {
+                "local": localversion,
+                "remote": remoteversion,
+                "has_update": has_update,
+            }
+        async_dispatcher_send(self.hass, SIGNAL_SENSOR_UPDATE)
 
-def get_local_version(card, lovelace_config):
-    """Return the local version if any."""
-    cardconfig = ''
-    with open(lovelace_config, 'r') as local:
-        for line in local.readlines():
-            if '/' + card + '.js' in line:
-                cardconfig = line
-                break
-    if '=' in cardconfig:
-        localversion = cardconfig.split('=')[1].split('\n')[0]
-        _LOGGER.debug('Local version of %s is %s', card, localversion)
-        return localversion
-    else:
+    def update_cards(self):
+        """Update all cards"""
+        for card in self.cards:
+            if self.hass.data[DATA_CC][card]['has_update']:
+                self.update_card(card)
+        async_dispatcher_send(self.hass, SIGNAL_SENSOR_UPDATE)
+
+    def update_card(self, card):
+        """Update one cards"""
+        if card in self.hass.data[DATA_CC]:
+            if self.hass.data[DATA_CC][card]['has_update']:
+                self.download_card(card)
+                self.update_resource_version(card)
+                self.hass.data[DATA_CC][card]['local'] = self.hass.data[DATA_CC][card]['remote']
+                self.hass.data[DATA_CC][card]['has_update'] = False
+                _LOGGER.info('Upgrade of %s from version %s to version %s complete', card, self.hass.data[DATA_CC][card]['local'], self.hass.data[DATA_CC][card]['remote'])
+        else:
+            _LOGGER.warn('Upgrade failed, no valid card specified %s', card)
+
+    def download_card(self, card):
+        """Downloading new card"""
+        _LOGGER.debug('Downloading new version of %s', card)
+        downloadurl = BASE_REPO + card + '/' + card + '.js'
+        response = requests.get(downloadurl)
+        if response.status_code == 200:
+            with open(self.www_dir + card + '.js', 'wb') as card_file:
+                card_file.write(response.content)
+
+    def update_resource_version(self, card):
+        """Updating the ui-lovelace file"""
+        localversion = self.hass.data[DATA_CC][card]['local']
+        remoteversion = self.hass.data[DATA_CC][card]['remote']
+        _LOGGER.debug('Updating configuration for %s', card)
+        sedcmd = 's/\/'+ card + '.js?v=' + str(localversion) + '/\/'+ card + '.js?v=' + str(remoteversion) + '/'
+        _LOGGER.debug('Upgrading card in config from version %s to version %s', localversion, remoteversion)
+        subprocess.call(["sed", "-i", "-e", sedcmd, self.lovelace_config])
+
+    def get_installed_cards(self):
+        """Get all cards in use from the www dir"""
+        _LOGGER.debug('Checking for installed cards in  %s', self.www_dir)
+        cards = []
+        cards_in_use = []
+        for file in os.listdir(self.www_dir):
+            if file.endswith(".js"):
+                cards.append(file.split('.')[0])
+        if len(cards):
+            _LOGGER.debug('Checking which cards that are in use in ui-lovelace.yaml')
+            for card in cards:
+                with open(self.lovelace_config, 'r') as local:
+                    for line in local.readlines():
+                        if '/' + card + '.js' in line:
+                            cards_in_use.append(card)
+                            break
+            _LOGGER.debug('These cards where found: %s', cards_in_use)
+        else:
+            _LOGGER.debug('No cards where found. %s', cards)
+            cards_in_use = None
+        return cards_in_use
+
+    def get_remote_version(self, card):
+        """Return the remote version if any."""
+        remoteversion = BASE_REPO + card + '/VERSION'
+        response = requests.get(remoteversion)
+        if response.status_code == 200:
+            remoteversion = response.text
+            _LOGGER.debug('Remote version of %s is %s', card, remoteversion)
+            remoteversion = remoteversion
+        else:
+            _LOGGER.debug('Could not get the remote version for %s', card)
+            remoteversion = False
+        return remoteversion
+
+    def get_local_version(self, card):
+        """Return the local version if any."""
+        cardconfig = ''
+        with open(self.lovelace_config, 'r') as local:
+            for line in local.readlines():
+                if '/' + card + '.js' in line:
+                    cardconfig = line
+                    break
+        if '=' in cardconfig:
+            localversion = cardconfig.split('=')[1].split('\n')[0]
+            _LOGGER.debug('Local version of %s is %s', card, localversion)
+            return localversion
         _LOGGER.debug('Could not get the local version for %s', card)
         return False
-
-def download_card(card, www_dir):
-    """Downloading new card"""
-    _LOGGER.debug('Downloading new version of %s', card)
-    downloadurl = BASE_REPO + card + '/' + card + '.js'
-    response = requests.get(downloadurl)
-    if response.status_code == 200:
-        with open(www_dir + card + '.js', 'wb') as card_file:
-            card_file.write(response.content)
-
-def update_config(lovelace_config, card, localversion, remoteversion):
-    """Updating the ui-lovelace file"""
-    _LOGGER.debug('Updating configuration for %s', card)
-    sedcmd = 's/\/'+ card + '.js?v=' + str(localversion) + '/\/'+ card + '.js?v=' + str(remoteversion) + '/'
-    _LOGGER.debug('Upgrading card in config from version %s to version %s', localversion, remoteversion)
-    subprocess.call(["sed", "-i", "-e", sedcmd, lovelace_config])
---
+++ custom_components/sensor/custom_cards.py
@@ -0,0 +1,56 @@
+"""
+Get updates about your custom_cards.
+
+For more details about this component, please refer to the documentation at
+https://github.com/custom-components/sensor.custom_cards
+"""
+from datetime import timedelta
+from homeassistant.helpers.entity import Entity
+from custom_components.custom_cards import DATA_CC, SIGNAL_SENSOR_UPDATE
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+import custom_components.custom_cards as cc
+
+__version__ = '0.0.5'
+
+DEPENDENCIES = ['custom_cards']
+
+SCAN_INTERVAL = timedelta(seconds=60)
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Create the sensor"""
+    add_devices([CustomCards(hass)])
+
+class CustomCards(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self, hass):
+        """Initialize the sensor."""
+        self.hass = hass
+        self._state = None
+        self._attributes = self.hass.data[DATA_CC]
+
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        async_dispatcher_connect(
+            self.hass, SIGNAL_SENSOR_UPDATE, self._update_callback)
+
+    def _update_callback(self):
+        """Method to update sensor value"""
+        self._state = 'Active'
+        self._attributes = self.hass.data[DATA_CC]
+        self.async_schedule_update_ha_state()
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Custom Card Tracker'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return 'Active'
+
+    @property
+    def device_state_attributes(self):
+        """Return attributes for the sensor."""
+        return self._attributes
